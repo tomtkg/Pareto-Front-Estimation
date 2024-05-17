9b791e01361b602dae2317e390c118e425efef6c
tomtkg, Ph.D. <tom.aquamarine@gmail.com>
2024-05-17 12:09:39 +0900

    update PFE.m

--- PFE.m
+++ PFE.m
@@ -9,5 +9,15 @@ function PFE
     
     Fig5to7(); % Pareto front estimation with three objective vector samplers
     Fig8to9(); % Pareto front estimation in m = 3 objective case
-    DTLZ7();   % Figure3(a) and Figure9(a)
+    DTLZ7();   % Figure 3(a) and Figure 9(a)
+
+    %% Doctoral Thesis
+    FigD1toD2();  % Example of response surface methodology and
+                  % the proposed Pareto front estimation procedure
+    FigD3toD4();  % Pareto front estimation in m = 2 objective case
+    FigD5();      % Pareto front estimation of Pareto fronts with different objective value ranges
+    
+    FigD6toD8();  % Pareto front estimation with three objective vector samplers
+    FigD9toD10(); % Pareto front estimation in m = 3 objective case
+    DDTLZ7();     % Figure 4(a) and Figure 10(a)
 end
